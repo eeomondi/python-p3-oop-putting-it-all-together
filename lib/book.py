933873bb9c14eff7270ccfae7ab3a68a4cb3918b
Elvis <elvis.omondi@student.moringaschool.com>
2024-10-17 15:32:01 +0300

    commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
-
 class Book:
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.is_checked_out = False
+
+    def check_out(self):
+        self.is_checked_out = True
+
+    def return_book(self):
+        self.is_checked_out = False
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.is_sold = False
+
+    def sell(self):
+        self.is_sold = True
+
+    def return_shoe(self):
+        self.is_sold = False
