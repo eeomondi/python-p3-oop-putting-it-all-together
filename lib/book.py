30cff448b09b3c50b8b0d41809ff89b1b10d8966
Elvis <elvis.omondi@student.moringaschool.com>
2024-10-17 16:40:23 +0300

    commit

--- lib/book.py
+++ lib/book.py
@@ -1,15 +1,22 @@
 # lib/book.py
+
 class Book:
-    def __init__(self, title, page_count):
+    def __init__(self, title, author, page_count=0):
         self.title = title
-        self.page_count = page_count  # Add this line
-        self.is_checked_out = False
+        self.author = author
+        self.page_count = page_count
+        self.is_checked_out = False  # Default value for the checkout status
 
     def check_out(self):
+        """Mark the book as checked out."""
         self.is_checked_out = True
 
     def return_book(self):
+        """Return the book and mark it as not checked out."""
         self.is_checked_out = False
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, {self.page_count} pages"
 
 
     
