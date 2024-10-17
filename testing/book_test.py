3f601123ae224819d1a68239759c8d01013c27a0
Elvis <elvis.omondi@student.moringaschool.com>
2024-10-17 15:42:27 +0300

    commit

--- testing/book_test.py
+++ testing/book_test.py
@@ -4,10 +4,12 @@ import pytest
 from lib.book import Book
 
 def test_book_creation():
-    book = Book("1984", "George Orwell")
+    book = Book("1984", "George Orwell", 328)  # Ensure you pass the page_count
     assert book.title == "1984"
     assert book.author == "George Orwell"
+    assert book.page_count == 328  # Add this assertion for page_count
     assert book.is_checked_out is False
+
 
 def test_check_out():
     book = Book("1984", "George Orwell")
